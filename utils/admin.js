dd4d59b6ff730e5f6dea6893e196f1ac844b6756
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-01-09 17:52:27 -0800

    add service account to env

--- utils/admin.js
+++ utils/admin.js
@@ -1,10 +1,13 @@
 import admin from "firebase-admin";
-import serviceAccount from "../serviceAccountKey.json";
+import dotenv from "dotenv";
+dotenv.config();
 
 if (!admin.apps.length) {
     try {
         admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
+            credential: admin.credential.cert(
+                JSON.parse(process.env.SERVICE_ACCOUNT_KEY)
+            ),
         });
     } catch (error) {
         console.log("Firebase admin initialization error", error.stack);
